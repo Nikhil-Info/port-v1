786791f2e2fb98d5c5299324166d484950faae94
Nikhil L <laudyanikhil@gmail.com>
2025-05-08 00:11:01 -0600

    added toggle menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,10 @@ const hamburger = document.querySelector('.hamburger')
 const navList = document.querySelector('.nav-list')
 const socials = document.querySelector('.social-nav')
 const navLinks = document.querySelectorAll('.nav-links')
+
+const toggleMobileMenu = () => {
+	hamburger.classList.toggle('open')
+	navList.classList.toggle('open')
+	socials.classList.toggle('open')
+	document.body.classList.toggle('open')
+}
